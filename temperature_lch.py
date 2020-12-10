b1b6808ad70491eaabcd62765efc5a5a0723c484
oliveruhlar <oliver.uhlar@pantheon.tech>
2020-12-10 08:18:37 +0100

    final

--- temperature_lch.py
+++ temperature_lch.py
@@ -11,7 +11,7 @@ def yaml_loader(filepath):
 
 yaml_data = yaml_loader('bb_temp.yml')
 
-x_data = ['January','February','March','April','May','Jun','July','August','September','October','November','December']
+x_data = ['January','February','March','April','May','June','July','August','September','October','November','December']
 y_high = [1.0,4.4,9.5,15.2,20.9,23.6,25.5,25.8,20.6,14.6,6.5,1.9]
 y_low = [-6.7,-5.2,-1.5,2.4,7.2,10.1,11.5,11.1,None,3.4,-0.8,-4.6]
 y_avg = [-2.9,-0.8,3.5,8.7,14.0,16.9,18.5,18.0,13.5,8.2,2.7,-1.4]
